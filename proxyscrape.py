978650efa0e2a156d5cc11ec937c926fc6b4678d
dev-zarir <shahriar4iso@gmail.com>
2023-09-29 10:14:09 +0600

    Update

--- proxyscrape.py
+++ proxyscrape.py
@@ -4,5 +4,14 @@ from string import ascii_letters, digits
 
 def get_session():
     st=''.join(choices(ascii_letters+digits, k=randint(8,20)))
-    return f'http://73738zbpcibedc0-session-{st}-lifetime-10:okjz6fk9nkkx3rc@rp.proxyscrape.com:6060'
+    return f'http://73738zbpcibedc0-session-{st}-lifetime-5:okjz6fk9nkkx3rc@rp.proxyscrape.com:6060'
 
+if __name__ == '__main__':
+    pr = get_session()
+    print(pr)
+    
+    r=get('https://ip.oxylabs.io', proxies={'http':pr,'https':pr})
+    print(r.text)
+    
+    
+    
