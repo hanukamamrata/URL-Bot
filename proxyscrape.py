7d7ff89ba53c0746965e56871b41030006d3bda1
dev-zarir <shahriar4iso@gmail.com>
2023-10-10 18:38:52 +0600

    Update

--- proxyscrape.py
+++ proxyscrape.py
@@ -1,11 +1,14 @@
 from requests import get
-from random import choices, randint
+from random import choices, randint, choice
 from string import ascii_letters, digits
 import requests
 
 def get_session():
+    countries = ['us', 'gb', 'au', 'ca', 'in', 'mx', 'nz']
+    country = choice(countries) 
     st = ''.join(choices(ascii_letters+digits, k=randint(8,20)))
-    pr = f'http://73738zbpcibedc0-session-{st}-lifetime-10:okjz6fk9nkkx3rc@rp.proxyscrape.com:6060'
+    #      http://73738zbpcibedc0-country-in-session-v0ntago2ly-lifetime-10:okjz6fk9nkkx3rc@rp.proxyscrape.com:6060
+    pr = f'http://73738zbpcibedc0-country-{country}-session-{st}-lifetime-10:okjz6fk9nkkx3rc@rp.proxyscrape.com:6060'
     try:
         get('http://cloudflare.com', proxies=dict(http=pr,https=pr), timeout=10)
     except requests.exceptions.ReadTimeout:
