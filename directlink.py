ed8402b21d74662a2feda2adf75f1b0c251cb995
dev-zarir <shahriar4iso@gmail.com>
2023-10-15 18:32:06 +0600

    Update

--- directlink.py
+++ directlink.py
@@ -1,9 +1,82 @@
-import seleniumwire.undetected_chromedriver as uc
+import undetected_chromedriver as uc
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 def run_directlink_bot(link, proxy=None, headless=True):
+    try: __file__
+    except: __file__ = os.getcwd()
+    
+    base_dir=path.dirname(path.abspath(__file__))
+    prext_dir=path.join(base_dir, 'pr_ext')
+    
+    if proxy:
+        up, hp = proxy.replace('http://','').replace('https://','').split('@')
+        username, password = up.split(':')
+        host, port = hp.split(':')
+        
+        try:
+            os.mkdir(prext_dir)
+        except FileExistsError:
+            pass
+        
+        manifest_json = """
+            {
+                "version": "1.0.0",
+                "manifest_version": 2,
+                "name": "Proxies",
+                "permissions": [
+                    "proxy",
+                    "tabs",
+                    "unlimitedStorage",
+                    "storage",
+                    "<all_urls>",
+                    "webRequest",
+                    "webRequestBlocking"
+                ],
+                "background": {
+                    "scripts": ["background.js"]
+                },
+                "minimum_chrome_version":"22.0.0"
+            }
+        """
+        
+        background_js = """
+            var config = {
+                    mode: "fixed_servers",
+                    rules: {
+                    singleProxy: {
+                        scheme: "http",
+                        host: "%s",
+                        port: parseInt(%s)
+                    },
+                    bypassList: ["localhost"]
+                    }
+                };
+        
+            chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+        
+            function callbackFn(details) {
+                return {
+                    authCredentials: {
+                        username: "%s",
+                        password: "%s"
+                    }
+                };
+            }
+        
+            chrome.webRequest.onAuthRequired.addListener(
+                        callbackFn,
+                        {urls: ["<all_urls>"]},
+                        ['blocking']
+            );
+        """ % (host, port, username, password)
+        
+        with open(path.join(prext_dir, "manifest.json"), 'w') as m_file:
+            m_file.write(manifest_json)
+        with open(path.join(prext_dir, "background.js"), 'w') as b_file:
+            b_file.write(background_js)
+    
     options = uc.ChromeOptions()
     if headless:
         options.add_argument('--headless=new')
@@ -13,9 +86,11 @@ def run_directlink_bot(link, proxy=None, headless=True):
     options.add_argument("--remote-debugging-port=9222")
     options.add_argument('--ignore-ssl-errors=yes')
     options.add_argument('--ignore-certificate-errors')
-    options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2}) # Block all Image
+    options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2}) # Block all images
+    if proxy:
+        options.add_argument(f'--load-extension={prext_dir}')
     
-    driver = uc.Chrome(version_main=117, options=options, seleniumwire_options=dict(proxy=dict(http=proxy, https=proxy)))
+    driver = uc.Chrome(version_main=117, options=options)
     driver.maximize_window()
     driver.get(link)
     if 'Anonymous Proxy detected' in driver.page_source:
