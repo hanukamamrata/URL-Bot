64493a2f4f63d5be106e2893c242026b6bd433fe
dev-zarir <shahriar4iso@gmail.com>
2023-10-16 18:23:52 +0600

    Update

--- all_links.py
+++ all_links.py
@@ -3,6 +3,7 @@ from random import randint, choice
 random_v2links = f'http://vlshort.com/tzpost-{randint(1,70)}'
 
 random_directlink = choice([
+    'https://www.highcpmrevenuegate.com/zgb2u2g6y0?key=32b60498c1c0e7a35409a53d7c884a0b',
     'https://www.highcpmrevenuegate.com/jzchfepg71?key=c78eab876d3b58828abcc71249078fc1',
     'https://www.highcpmrevenuegate.com/uwna3zpeyw?key=25df35ae5552407727f454c20ec60fa8'
 ])
