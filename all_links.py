61390ed1fdc917f8c27b4b0fd9be8753fd9a9249
dev-zarir <shahriar4iso@gmail.com>
2023-10-17 08:15:42 +0600

    Update

--- all_links.py
+++ all_links.py
@@ -7,8 +7,7 @@ random_shrinkforearn = f'http://shrinkforearn.xyz/tzpost-{randint(1,70)}'
 random_directlink = choice([
     'https://www.highcpmrevenuegate.com/zgb2u2g6y0?key=32b60498c1c0e7a35409a53d7c884a0b',
     'https://www.highcpmrevenuegate.com/jzchfepg71?key=c78eab876d3b58828abcc71249078fc1',
-    'https://www.highcpmrevenuegate.com/uwna3zpeyw?key=25df35ae5552407727f454c20ec60fa8'
+    'https://www.highcpmrevenuegate.com/uwna3zpeyw?key=25df35ae5552407727f454c20ec60fa8',
+    'https://www.highcpmrevenuegate.com/vt01utgb?key=6ca6ca076f75f77c7f7d4e79f0892a78'
 ])
-
-
 
