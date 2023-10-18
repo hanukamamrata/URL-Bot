6f1dec12c74c54f6643af9a426a069f2bdb1c1b9
dev-zarir <shahriar4iso@gmail.com>
2023-10-18 09:36:57 +0600

    Update

--- shrinkforearn.py
+++ shrinkforearn.py
@@ -5,12 +5,17 @@ from bs4 import BeautifulSoup
 def run_shrinkforearn_bot(link:str, proxy=None, headless=None):
     base_domain = 'shrinkforearn.xyz'
     replace_with = 'shrinkforearn.in'
+    org_refer = 'https://technicalzarir.blogspot.com'
     refer_link = 'https://wp.uploadfiles.in'
     s=Session()
     if proxy:
         s.proxies={'http':proxy,'https':proxy}
     s.cookies.set('ab','2', domain=replace_with.split('/')[0])
     ua='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+    hl=link
+    while hl:
+        hl=s.get(hl, headers={'Referer': org_refer, 'User-Agent': ua}, allow_redirects=False).headers.get('Location')
+        
     r1=s.get(link.replace(base_domain, replace_with), headers={'User-Agent': ua, 'Referer': refer_link})
     html=r1.text
     soap=BeautifulSoup(html, 'html.parser')
