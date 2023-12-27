12805c7601e36db10f1dcb1a698f111091c3fc0d
zarir94 <shahriar@gmail.com>
2023-12-27 19:31:04 +0600

    Update

--- magiclinks.py
+++ magiclinks.py
@@ -1,17 +1,24 @@
 from cloudscraper import CloudScraper as Session
-import re
+from bs4 import BeautifulSoup
+import re, time
 
 def run_magic_bot(link, proxy=None, headless=None):
     s=Session()
     s.proxies=dict(http=proxy, https=proxy)
+    s.cookies.set('ab', '2', domain='magiclink1.com')
+    r0=s.get(link, headers={'Referer': 'https://technicalzarir.blogspot.com/'})
     r1=s.get(link, headers={'Referer': 'https://forextrading.govaf.com/'})
-    open('test.html', 'w').write(r1.text)
+    doc=BeautifulSoup(r1.text, 'html.parser')
+    form = doc.select_one('form')
+    all_inputs = { k.get('name') : k.get('value') for k in form.select('input[value]')}
+    time.sleep(2)
+    r2 = s.post('https://magiclink1.com/links/go', data=all_inputs, headers={'X-Requested-With': 'XMLHttpRequest'})
+    print('Magic Links:', r2.text)
     
 
 
 if __name__=='__main__':
     from all_links import random_magic
-    print(random_magic)
     run_magic_bot(random_magic, headless=False)
 
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from all_links import *
 from adrinolinks import run_adrino_bot
+from magiclinks import run_magic_bot
 from nanolinks import run_nano_bot
 from random import randint
 import urllib3, threading
@@ -16,6 +17,7 @@ def main(proxy=None, **kw):
     t=[]
     #t.append(Thread(target=lambda: run_adrino_bot(random_adrino, proxy, **kw)))
     t.append(Thread(target=lambda: run_nano_bot(random_nanolinks, proxy, **kw)))
+    t.append(Thread(target=lambda: run_magic_bot(random_magic, proxy, **kw)))
     
     for v in t:
         v.start()
