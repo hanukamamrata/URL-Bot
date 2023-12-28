a5c9f209c2a8a1db513968b79db12e322d7e0ca5
zarir94 <shahriarzariracc@gmail.com>
2023-12-28 13:04:34 +0600

    Fixed AdrinoLinks

--- adrinolinks.py
+++ adrinolinks.py
@@ -1,13 +1,36 @@
 from cloudscraper import CloudScraper as Session
-import re
+from DrissionPage import ChromiumPage, ChromiumOptions
+import os, time
 
-def run_adrino_bot(link, proxy=None, headless=None):
+def old_run_adrino_bot(link, proxy=None, headless=None):
     s=Session()
     s.proxies=dict(http=proxy, https=proxy)
     r1=s.get(link, headers={'Referer': 'https://technicalzarir.blogspot.com'}, allow_redirects=False)
-    print(r1.text)
     print('Adrino Links:', r1.headers.get('Location'))
     
+    
+def run_adrino_bot(link, proxy=None, headless=None):
+    # Avoid Proxy else CloudFlare will eat all data
+    options = ChromiumOptions()
+    options.add_extension(os.path.join(os.path.dirname(__file__), 'header_modifier'))
+    options.add_extension(os.path.join(os.path.dirname(__file__), 'NopeCha'))
+    options.auto_port()
+    page = ChromiumPage(options)
+    # page.get('http://httpbin.org/headers')
+    # time.sleep(50)
+    page.get(link)
+    attempt = 0
+    while 1:
+        time.sleep(3)
+        if 'technicalzarir.blogspot.com' in page.url:
+            break
+        attempt+=1
+        if attempt >= 13:
+            page.quit()
+            raise Exception('40 sec passed but cloudflare is still here.')
+    
+    print('Adrino Links:', page.url)
+    page.quit()
 
 
 if __name__=='__main__':
