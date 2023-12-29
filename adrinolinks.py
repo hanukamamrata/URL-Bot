e54a53b4b5a7fc494154e3657a68e7f8a6597e35
zarir94 <shahriar@gmail.com>
2023-12-29 12:45:52 +0600

    Update

--- adrinolinks.py
+++ adrinolinks.py
@@ -13,26 +13,28 @@ def old_run_adrino_bot(link, proxy=None, headless=None):
 def run_adrino_bot(link, proxy=None, headless=None):
     # Avoid Proxy else CloudFlare will eat all data
     options = ChromiumOptions()
-    options.add_extension(os.path.join(os.path.dirname(__file__), 'header_modifier'))
+    #options.add_extension(os.path.join(os.path.dirname(__file__), 'header_modifier'))
     options.add_extension(os.path.join(os.path.dirname(__file__), 'NopeCha'))
     options.auto_port()
     page = ChromiumPage(options)
-    page.set.headers={'X-Forwarded-For': generate_random_ip()}
-    # page.get('http://httpbin.org/headers')
-    # time.sleep(50)
     page.get(link)
     attempt = 0
     while 1:
         time.sleep(3)
-        if 'technicalzarir.blogspot.com' in page.url:
+        if not 'Just a moment' in page.html:
             break
         attempt+=1
         if attempt >= 13:
             page.quit()
             raise Exception('40 sec passed but cloudflare is still here.')
     
-    print('Adrino Links:', page.url)
+    cookies = page.cookies
+    ua = page.user_agent
     page.quit()
+    
+    s=Session()
+    r1=s.get(link, headers={'User-Agent': ua, 'Referer': 'https://technicalzarir.blogspot.com', 'X-Forwarded-For': generate_random_ip()}, cookies=cookies, allow_redirects=False)
+    print('Adrino Links:', r1.headers.get('Location'))
 
 
 if __name__=='__main__':
