1cbd3816e2e4f05f0d62957dae7eaeeb515a72a4
zarir94 <shahriar@gmail.com>
2024-01-29 09:15:35 +0600

    Update

--- proxyscrape.py
+++ proxyscrape.py
@@ -7,7 +7,7 @@ def get_session():
     countries = ['us', 'gb', 'au', 'ca', 'in', 'mx', 'nz']
     country = choice(countries) 
     st = ''.join(choices(ascii_letters+digits, k=randint(8,20)))
-    pr = f'http://73738zbpcibedc0-country-{country}-session-{st}-lifetime-10:okjz6fk9nkkx3rc@rp.proxyscrape.com:6060'
+    pr = f'http://ieyqmoirvvpfdh6-country-{country}-session-{st}-lifetime-10:7dvv9jwswj601ad@rp.proxyscrape.com:6060'
     #try:
     #    get('http://google.com', proxies=dict(http=pr,https=pr), timeout=10)
     #except requests.exceptions.ReadTimeout:
