b9a35567bfb9aebf2c466e72c165d7b217e1f9c4
zarir94 <shahriar@gmail.com>
2024-04-07 23:08:26 +0600

    Update

--- all_links.py
+++ all_links.py
@@ -4,7 +4,7 @@ random_v2links = f'http://vlshort.com/tzpost-{randint(1,70)}'
 
 random_udlinks = f'https://www.udlinks.com/tzpost-{randint(1,70)}'
 
-random_telegramlinks = f'https://telegramlink.in/mlw-{randint(1,100 + 1)}'
+random_telegramlinks = f'https://telegramlink.in/{choice(["CMwDm1", "2mlYqj2", "PzCEFw3", "1ucg4", "kJaqu5", "lRWI6", "IxBH07", "9tLbem8", "vAft9", "XGXow10", "970da11", "R5eTX12", "ouCp213", "syeF14", "cuCjB15", "yYrEG16", "hqVJK17", "dI7CBi18", "L2cDK19", "qpoOTQ20", "XXIj21", "QHgrB22", "MnKq3N23", "F1PSyD24", "ucSTn25", "fMmi26", "dHG727", "9piFQS28", "cRnOeM29", "anZU830", "BavCk31", "mkyI32", "TaVGKs33", "ytSRwB34", "CyY3kY35", "eQqV36", "E3zv37", "sRwlr38", "0WmR39", "hzct40", "Bo8p841", "TtHl4S42", "zKMGY43", "mYUB44", "uFmXG45", "Vzke46", "og9sw47", "ESoaA48", "NkBs49", "nlOQ50", "rvCkq51", "CdKX52", "sYuNe53", "N7JQh54", "2Sph5V55", "dExhSQ56", "udgP057", "NKbL8C58", "0zWhQK59", "nylNm60", "MJCHX61", "81Y9yO62", "vSty63", "Zl7NC64", "VvnlkP65", "pSueYL66", "4kgZG67", "EdEW68", "mEmqOB69", "NxgF570", "cCDAnt71", "8KJGft72", "vAl4Q73", "2ijg8974", "bio8x75", "oeUSRk76", "YiRp77", "IZsc78", "L5ukwg79", "oF2R080", "XiLWa881", "mSqmH82", "WOOoEB83", "nKWQkw84", "xnR685", "ZoXS86", "oKVmsS87", "hb1j288", "5Hm789", "1XvBKq90", "b0U2H391", "TPAy92", "8wgB93", "aCVxLo94", "PHVLyD95", "BZwyF96", "Do0z97", "9rcL98", "rFxCh99", "p3D9100"])}'
 
 random_adrino = f'https://amritadrino.com/includes/open.php?id={choice(list(range(2425906, 2425910)) + list(range(2425911, 2425977)))}'
 
